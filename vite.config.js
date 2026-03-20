baf29af060be9ef1c02f301648753a5a99eb6b29
Alan Joel Murillo <171400674+AlanJoelMurillo@users.noreply.github.com>
2026-03-19 18:34:32 -0600

    Add server configuration for Vite
    Added server configuration for hosting and hot-reload.

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    // --- AÑADE ESTA SECCIÓN ---
+    server: {
+        host: '0.0.0.0', // Escucha en todas las interfaces del contenedor
+        hmr: {
+            host: 'localhost', // El navegador busca el hot-reload en tu máquina
+        },
+    },
+    // --------------------------
 });
